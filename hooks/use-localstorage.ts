d0f983782c1b2025453786c09fc095e16f554039
Nick Nance <nance.nick@gmail.com>
2024-09-28 08:03:13 -0500

    store api keys in local storage

---
+++ hooks/use-localstorage.ts
@@ -0,0 +1,35 @@
+import { SetStateAction, useState } from "react";
+
+// Custom hook for managing settings in local storage
+export const useLocalStorage = (
+  key: string,
+  initialValue: Record<string, string>
+) => {
+  const [storedValue, setStoredValue] = useState<Record<string, string>>(() => {
+    if (typeof window === "undefined") {
+      return initialValue;
+    }
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value: SetStateAction<Record<string, string>>) => {
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      if (typeof window !== "undefined") {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue] as const;
+};
