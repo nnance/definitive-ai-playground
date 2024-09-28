c624363b7471365d06979b12d23dd2a6d99f92a1
Nick Nance <nance.nick@gmail.com>
2024-09-27 21:47:20 -0500

    remove additional header and navigation

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,20 +1,6 @@
 "use client";
 
-import {
-  Bird,
-  Book,
-  Bot,
-  Code2,
-  LifeBuoy,
-  Rabbit,
-  Settings,
-  Settings2,
-  Share,
-  SquareTerminal,
-  SquareUser,
-  Triangle,
-  Turtle,
-} from "lucide-react";
+import { Bird, Rabbit, Settings, Turtle } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -35,133 +21,7 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
 import { Chatbot } from "@/components/chatbot";
-
-function SideBar() {
-  return (
-    <aside className="inset-y fixed  left-0 z-20 flex h-full flex-col border-r">
-      <div className="border-b p-2">
-        <Button variant="outline" size="icon" aria-label="Home">
-          <Triangle className="size-5 fill-foreground" />
-        </Button>
-      </div>
-      <nav className="grid gap-1 p-2">
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Button
-              variant="ghost"
-              size="icon"
-              className="rounded-lg bg-muted"
-              aria-label="Playground"
-            >
-              <SquareTerminal className="size-5" />
-            </Button>
-          </TooltipTrigger>
-          <TooltipContent side="right" sideOffset={5}>
-            Playground
-          </TooltipContent>
-        </Tooltip>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Button
-              variant="ghost"
-              size="icon"
-              className="rounded-lg"
-              aria-label="Models"
-            >
-              <Bot className="size-5" />
-            </Button>
-          </TooltipTrigger>
-          <TooltipContent side="right" sideOffset={5}>
-            Models
-          </TooltipContent>
-        </Tooltip>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Button
-              variant="ghost"
-              size="icon"
-              className="rounded-lg"
-              aria-label="API"
-            >
-              <Code2 className="size-5" />
-            </Button>
-          </TooltipTrigger>
-          <TooltipContent side="right" sideOffset={5}>
-            API
-          </TooltipContent>
-        </Tooltip>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Button
-              variant="ghost"
-              size="icon"
-              className="rounded-lg"
-              aria-label="Documentation"
-            >
-              <Book className="size-5" />
-            </Button>
-          </TooltipTrigger>
-          <TooltipContent side="right" sideOffset={5}>
-            Documentation
-          </TooltipContent>
-        </Tooltip>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Button
-              variant="ghost"
-              size="icon"
-              className="rounded-lg"
-              aria-label="Settings"
-            >
-              <Settings2 className="size-5" />
-            </Button>
-          </TooltipTrigger>
-          <TooltipContent side="right" sideOffset={5}>
-            Settings
-          </TooltipContent>
-        </Tooltip>
-      </nav>
-      <nav className="mt-auto grid gap-1 p-2">
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Button
-              variant="ghost"
-              size="icon"
-              className="mt-auto rounded-lg"
-              aria-label="Help"
-            >
-              <LifeBuoy className="size-5" />
-            </Button>
-          </TooltipTrigger>
-          <TooltipContent side="right" sideOffset={5}>
-            Help
-          </TooltipContent>
-        </Tooltip>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Button
-              variant="ghost"
-              size="icon"
-              className="mt-auto rounded-lg"
-              aria-label="Account"
-            >
-              <SquareUser className="size-5" />
-            </Button>
-          </TooltipTrigger>
-          <TooltipContent side="right" sideOffset={5}>
-            Account
-          </TooltipContent>
-        </Tooltip>
-      </nav>
-    </aside>
-  );
-}
 
 function SettingsDrawer() {
   return (
@@ -396,32 +256,14 @@ function SettingsSidebar() {
   );
 }
 
-function Header() {
-  return (
-    <header className="sticky top-0 z-10 flex h-[57px] items-center gap-1 border-b bg-background px-4">
-      <h1 className="text-xl font-semibold">Playground</h1>
-      <SettingsDrawer />
-      <Button variant="outline" size="sm" className="ml-auto gap-1.5 text-sm">
-        <Share className="size-3.5" />
-        Share
-      </Button>
-    </header>
-  );
-}
-
 export default function Dashboard() {
   return (
-    <div className="grid h-screen w-full pl-[56px]">
-      <SideBar />
-      <div className="flex flex-col">
-        <Header />
-        <main className="grid flex-1 gap-4 overflow-auto p-4 md:grid-cols-2 lg:grid-cols-3">
-          <SettingsSidebar />
-          <div className="relative flex h-full min-h-[50vh] flex-col lg:col-span-2">
-            <Chatbot />
-          </div>
-        </main>
+    <main className="grid flex-1 gap-4 overflow-auto p-4 md:grid-cols-2 lg:grid-cols-3">
+      <SettingsSidebar />
+      <div className="relative flex h-full min-h-[50vh] flex-col lg:col-span-2">
+        <SettingsDrawer />
+        <Chatbot />
       </div>
-    </div>
+    </main>
   );
 }
